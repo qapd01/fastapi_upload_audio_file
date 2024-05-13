f70444480b2dd64df0b47d8e2ce1c4bce824b259
boss <bossxlp@gmail.com>
2024-05-13 15:16:47 +0700

    fix: change to upload multi-part file

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from typing import Annotated
 
-from fastapi import FastAPI, File, Request
+from fastapi import FastAPI, File, Request, Form, UploadFile
 from fastapi.templating import Jinja2Templates
 from starlette.templating import _TemplateResponse
 
@@ -20,8 +20,25 @@ async def get_form_upload_trading_ideas(request: Request) -> _TemplateResponse:
 
 
 @app.post("/upload-file")
-def read_item(file: Annotated[bytes, File()]):
+def read_item(
+        file: Annotated[UploadFile, File()],
+        file2: Annotated[UploadFile, File()],
+        name: Annotated[str, Form()]
+):
     print(file)
+    print(file2)
+    print(name)
     with open("uploaded_audio.wav", "wb") as audio_file:
-        audio_file.write(file)
+        audio_file.write(file.file.read())
     return {"status": "File saved successfully"}
+
+
+@app.post("/uploadfiles")
+async def create_upload_files(files: list[UploadFile]):
+    uploaded_files = []
+    for file in files:
+        contents = await file.read()
+        with open(file.filename, "wb") as f:
+            f.write(contents)
+        uploaded_files.append(file.filename)
+    return {"filenames": uploaded_files}
